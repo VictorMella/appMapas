b9ec67b8fde7a2e49d1f819104f1c1252884ede9
Victor <v.mellaq@gmail.com>
2021-10-29 15:38:42 -0300

    Avance

--- src/app/mapas/pages/zoom-range/zoom-range.component.ts
+++ src/app/mapas/pages/zoom-range/zoom-range.component.ts
@@ -1,27 +1,48 @@
-import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core'
+import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core'
 import * as mapboxgl from 'mapbox-gl'
+import { TargetLocator } from 'selenium-webdriver'
 @Component({
   selector: 'app-zoom-range',
   templateUrl: './zoom-range.component.html',
   styleUrls: ['./zoom-range.component.scss']
 })
-export class ZoomRangeComponent implements AfterViewInit {
+export class ZoomRangeComponent implements AfterViewInit, OnDestroy {
 
   @ViewChild('mapa') divMapa: ElementRef;
   mapa: mapboxgl.Map
-  zoonLabel: number
+  zoomLabel: number
+  center: [number, number]
 
   constructor() {
-    this.zoonLabel = 10
-   }
+    this.zoomLabel = 10
+    this.center = [-73.00349906408556, -36.744123074937555]
+  }
+
+  ngOnDestroy(): void {
+    this.mapa.off('zoom', () => {} )
+    this.mapa.off('zoomend', () => {} )
+    this.mapa.off('move', () => {} )
+  }
 
   ngAfterViewInit(): void {
     // (mapboxgl as any).accessToken = environment.mapBoxKey;
     this.mapa = new mapboxgl.Map({
       container: this.divMapa.nativeElement,
       style: 'mapbox://styles/mapbox/streets-v11',
-      center: [-73.00349906408556, -36.744123074937555],
-      zoom: this.zoonLabel, //Mientras mas alto el el numero mas cerca de la tierra se vera
+      center: this.center,
+      zoom: this.zoomLabel, //Mientras mas alto el el numero mas cerca de la tierra se vera
+    })
+
+    this.mapa.on('zoom', () => this.zoomLabel = Math.round(this.mapa.getZoom()))
+    this.mapa.on('zoomend', () => {
+      if (this.mapa.getZoom() > 20) {
+        this.mapa.zoomTo(20)
+      }
+    })
+    this.mapa.on('move', (eve) => {
+      const target = eve.target
+      const { lng, lat } = target.getCenter()
+      this.center = [ lng, lat ]
     })
     // Add map controls
     // this.mapa.addControl(new mapboxgl.NavigationControl())
@@ -29,11 +50,14 @@ export class ZoomRangeComponent implements AfterViewInit {
 
   zoomIn(): void {
     this.mapa.zoomIn()
-    this.zoonLabel = Math.round(this.mapa.getZoom())
 
   }
   zoomOut(): void {
     this.mapa.zoomOut()
-    this.zoonLabel = Math.round(this.mapa.getZoom())
+  }
+
+  changeZoomLabel(n: number) {
+    this.mapa.zoomTo( n )
+
   }
 }
